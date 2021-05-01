578c490ee3febb6d6fe421b36dc221c618ce1c4c
gogi999 <g.petrovic9@gmail>
2021-05-01 11:21:34 +0200

    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(bodyParser.json())
 // Helmet helps you secure your Express apps by setting various HTTP headers
 app.use(helmet());
+app.use(cors());
 
 router(app);
 
